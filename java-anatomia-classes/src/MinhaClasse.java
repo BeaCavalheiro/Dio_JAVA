4c7a4dde1b8e9163f285275241e2e322090648f0
Bea Cavalheiro <biacavalheiro2bia@gmail.com>
2023-01-20 13:23:09 -0300

    anatomia das classes1,2 e 3

---
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,26 @@
+public class MinhaClasse {
+
+    public static void main(String[] args) {
+        // variavel tem tipo+nome+valor*(valor não é obrigatorio)
+        // nome de variaveis podem ser iniciados com letras(maiusculas se for uma final,
+        // kamelCase se for var.)
+
+        String nome = "beatriz";
+        final String SOBRE_NOME = "souza cavalheiro"; // usar o termo final ou o nome da classe todo em maiuscula defini
+                                                      // que é uma variavel imutavel
+        int _idade = 29;
+        String $sexo = "feminino";
+        final String NOME_COMPLETO = nomeCompleto(nome, SOBRE_NOME);
+
+        System.out.println(NOME_COMPLETO);
+
+    }
+
+    // diferente do static void que nao retorna nada, apenas executa, metodos de
+    // retorno nao tem void e exigen um return
+    public static String nomeCompleto(String nome, String SOBRE_NOME) {
+        // metodo concat, é o mesmo que concatenar coisas
+        return nome.concat(" ").concat(SOBRE_NOME);
+    }
+
+}
