3a93f0ed853fc248fe008d3188ff12d58ccf5e85
Bea Cavalheiro <biacavalheiro2bia@gmail.com>
2023-01-31 17:12:45 -0300

    aula 04 oper

--- OPERADORES/src/Operadores.java
+++ OPERADORES/src/Operadores.java
@@ -3,10 +3,22 @@ public class Operadores {
         int num = 5; // num inicialmente é atribuido positivo
         System.out.println(num);// =5
 
-        num = - num; // num recebe ele mesmo da forma negativa
-        System.out.println(num); //=-5
+        num = -num; // num recebe ele mesmo da forma negativa
+        System.out.println(num); // =-5
 
-        num = - num; //para positivar é preciso multiplicar por -1 ou atribuir negatividade ao numero negativo afinal - com - = +
+        num = -num; // para positivar é preciso multiplicar por -1 ou atribuir negatividade ao
+                    // numero negativo afinal - com - = +
         System.out.println(num);// =5
+    }
+
+    public void Incremento(int num) {
+        num++; // primeiro imprime depois soma
+        ++num; // primeiro soma depois imprime
+    }
+
+    public void Decremento(int num) {
+        --num; // primeiro decrementa depois imprimi
+        num--; // primeiro imprimi depois decrementa
+
     }
 }
